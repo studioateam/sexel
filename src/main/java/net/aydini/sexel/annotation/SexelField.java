5b0bdf89491dc1bc8dc4175f2d13d52e91bc9760
Aydin Nasrollahpour <paakro@gmail.com>
2021-08-05 23:09:46 +0430

    remove celltype

--- src/main/java/net/aydini/sexel/annotation/SexelField.java
+++ src/main/java/net/aydini/sexel/annotation/SexelField.java
@@ -5,14 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.apache.poi.ss.usermodel.CellType;
-
+import net.aydini.mom.common.service.maper.Maper;
+import net.aydini.mom.util.mapper.ToStringMapper;
 import net.aydini.sexel.workbook.style.CellStyler;
 import net.aydini.sexel.workbook.style.DefaultCellStyler;
 import net.aydini.sexel.workbook.style.font.DefaultFontCreator;
 import net.aydini.sexel.workbook.style.font.FontCreator;
-import net.aydini.mom.common.service.maper.Maper;
-import net.aydini.mom.util.mapper.ToStringMapper;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
@@ -20,14 +18,12 @@ public @interface SexelField {
 	
 	int columnIndex();
 	
-	String headerTitle();
+	String headerTitle() default "";
 	
 	Class<? extends Maper<Object, String>> converter() default ToStringMapper.class;
 	
 	Class<? extends CellStyler> style() default DefaultCellStyler.class;
 	
 	Class<? extends FontCreator> font() default DefaultFontCreator.class;
-	
-	CellType  cellType() default CellType.STRING;
 	
 }
